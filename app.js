db8d5975dc3fa47dede6bcff19daeb51d6caac76
Abdul Moiz Shoro <abdulmoizshoro@gmail.com>
2021-03-28 10:16:15 -0700

    work compelete

---
+++ app.js
@@ -0,0 +1,53 @@
+//1- overwrite value while making new object using Object.assign
+// const obj = {
+//         firstName: 'Abdul',
+//         lastName: 'Moiz',
+//         rollNo: 'JP-0044',
+//     }
+    
+//     let obj1 = Object.assign({}, obj)
+//     console.log(obj)
+    
+//     obj1.rollNo = 'JP-0048'
+//     console.log(obj1)
+    
+
+//2- Nested Terminay Operator
+// let temperature = prompt("what is the current temperature")
+// let cool = "cool"
+// let warm = "warm"
+// function checking(){
+//  const type = prompt("What type of temperature? (hot/cold)") 
+//  return type 
+// }
+// let hot ="hot"
+// let cold="cold"
+// const weather = temperature == cool || temperature == warm ? checking()==hot ? "its summer" : "its winter" : "You should wear dresses for summer"
+// console.log(weather)
+
+
+
+// 3- for-in loop 
+// let obj={
+//     name:"Moiz",
+//     program:"BBA",
+//     university:"Iqra University",
+// }
+// for(const n in obj){
+//     console.log(`${n} : ${obj[n]}`)
+// }
+
+
+// 4- for of loop
+
+// let arr=["waleed","moiz","huzaifa","anas"]
+// for(const n of arr){
+//     console.log(n);
+// }
+
+
+//4-  What is arrow function?
+
+// Defination:
+// An arrow function expression is a compact alternative to a traditional function expression,
+// but is limited and can't be used in all situations.
